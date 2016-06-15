29f89e115a280596ef228d1481b56f00fd1a1bff
huynq-tk-ios3 <huynq.work@gmail.com>
2016-06-15 12:01:10 +0700

    Added Instructor Detail Screen

---
+++ ERP/ERP/InstructorDetailViewController.swift
@@ -0,0 +1,42 @@
+//
+//  InstructorDetailViewController.swift
+//  ERP
+//
+//  Created by admin on 6/15/16.
+//  Copyright © 2016 Techkids. All rights reserved.
+//
+
+import UIKit
+
+class InstructorDetailViewController: UIViewController {
+
+    var instructor : Instructor?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    
+    func viewInstructorInfo() {
+        
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
